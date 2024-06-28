f2ed61a113f4291b74be832ca2ecd62638d48416
s1522711 <52498188+s1522711@users.noreply.github.com>
2024-06-28 17:19:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function testPost() {
     let postInput = document.getElementsByName("glitInput")[0];
     postInput.value = "test";
     document.forms[1].submit();
+    console.log("test");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
